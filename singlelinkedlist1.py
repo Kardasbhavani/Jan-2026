71e4347155519e7f8266de9149af21ad1ff8e3e2
Kardasbhavani <bhavanikardas@gmail.com>
2026-02-10 21:31:03 +0530

    Add single linked list implementation
    Implement a function to create a single linked list from an array and print the first element.

---
+++ singlelinkedlist1.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self,val):
+        self.data=val
+        self.next=None
+def createsinglelinkedlist(arr):
+    head=None
+    for val in arr:
+          if(head==None):
+            head=Node(val)
+            temp=head
+          else:
+            temp.next=Node(val) 
+            temp=temp.next
+    return head.next.data    
+arr=list(map(int,input().split()))
+print(createsinglelinkedlist(arr))
